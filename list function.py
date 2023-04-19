3ffce53c30aa0832e9b412b93ba0440ffb39d866
Krishna2-3 <113293578+Krishna2-3@users.noreply.github.com>
2023-04-19 23:13:04 +0530

    list function.py

---
+++ list function.py
@@ -0,0 +1,13 @@
+l1 = [1, 8, 8, 4, 3, 15, 20, 25, 89, 65,88]  # l1 is a list
+print(l1)
+l1.sort()
+print(l1)  # it is used for sorting list sorting
+l1.reverse() # it is used to reverse all elements of list
+print(l1)
+l1.append(1) #it is used to append list
+l1.append(0)
+print(l1)
+l1.remove(0) #it is used to remove element of list
+print(l1)
+l1.pop(8) #it is used to remove element of list with the help of index number.
+print(l1) #pop(8) means remove element of index number 8
