4fbb7b9dfacbac2f715e46b8c3bc55c7e78bc95e
Krishna2-3 <113293578+Krishna2-3@users.noreply.github.com>
2023-04-27 15:26:13 +0530

    Create Design6.py

---
+++ Design6.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+bgcolor('black')
+
+tracer (100)
+
+colour = ['cyan', 'white'] 
+
+for i in range(1200):
+
+	color(colour[i%2])	up()
+
+	fd(i)
+
+	down()
+
+	rt(1)
+
+	for j in range(5):
+
+		fd(j*5)
+
+		lt(20000)
