459158d2cafe393cd2a1d1bd9e1e0b6ad6ca3eca
Krishna2-3 <113293578+Krishna2-3@users.noreply.github.com>
2023-05-04 23:38:58 +0530

    Create Design15.py

---
+++ Design15.py
@@ -0,0 +1,47 @@
+import colorsys
+
+from turtle import *
+
+bgcolor('black')
+
+tracer(100)
+
+pensize(8)
+
+h = 0.5
+
+n=100
+
+for i in range(600):
+
+	c=colorsys.hsv_to_rgb(h,1,0.9)
+
+	h += 0.5/n
+
+	color(c)
+
+	fillcolor('black')
+
+	begin_fill()
+
+	circle(90,43)
+
+	circle(18)
+
+	rt(20)
+
+	bk(450)
+
+	up()
+
+	goto(0,0)
+
+	down()
+
+	circle(1,203)
+
+	rt(50)
+
+	end_fill()
+
+done()
