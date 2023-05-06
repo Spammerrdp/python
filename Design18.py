5633da5f5bb97222db76dbc446af6731479d3e05
Krishna2-3 <113293578+Krishna2-3@users.noreply.github.com>
2023-05-06 23:13:35 +0530

    Create Design18.py

---
+++ Design18.py
@@ -0,0 +1,25 @@
+from turtle import *
+
+speed('fastest')
+
+bgcolor("black")
+
+pencolor("cyan")
+
+fillcolor("purple")
+
+begin_fill()
+
+for i in range(40):
+
+	for j in range(4): 		forward(100)
+
+		right (90)
+
+	right (70)
+
+end_fill()
+
+hideturtle()
+
+done ()
