e9d090d2b27cef7ccf8a2a910d2f3243ef89beb3
Krishna2-3 <113293578+Krishna2-3@users.noreply.github.com>
2023-05-10 22:42:25 +0530

    Create Design23.py

---
+++ Design23.py
@@ -0,0 +1,31 @@
+from turtle import*
+
+import colorsys
+
+tracer (40)
+
+bgcolor('black')
+
+pensize (4)
+
+h=0.4
+
+for i in range (299):
+
+	c = colorsys.hsv_to_rgb (h,1,1)	color(c)
+
+	h+=0.002
+
+	rt(200)
+
+	for j in range (5):
+
+		fd(i)
+
+		rt(10)
+
+		rt(50)
+
+	rt(118)
+
+done()
