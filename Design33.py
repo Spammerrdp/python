3d4eb541fd2f7bc32ea6259eacb55ee694b1f9e1
Krishna2-3 <113293578+Krishna2-3@users.noreply.github.com>
2023-05-20 15:11:26 +0530

    Create Design33.py

---
+++ Design33.py
@@ -0,0 +1,34 @@
+from turtle import*
+
+from colorsys import *
+
+bgcolor('black')
+
+tracer(400)
+
+h = 0
+
+n = 210
+
+for i in range(2,290):
+
+		c = hsv_to_rgb(h,1,1)		
+    h += 120/n
+
+		color (c)
+
+		pensize(7)
+
+		up()
+
+		goto(0,0)
+
+		down()
+
+		circle(i,50)
+
+		for j in range(2,1,100):
+
+			circle(i/3,45)
+
+done()
