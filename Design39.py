1611e98de14e177d5ddaaa26be434508d055c7cb
Krishna2-3 <113293578+Krishna2-3@users.noreply.github.com>
2023-05-29 23:34:58 +0530

    Create Design39.py

---
+++ Design39.py
@@ -0,0 +1,26 @@
+from turtle import*
+
+import colorsys 
+
+bgcolor("black")
+
+tracer(2)
+
+n = 36
+
+h = 0.5
+
+for i in range (200):
+
+	k= colorsys.hsv_to_rgb (h,0.9,1)	
+  h+=1/n
+
+	color(k)
+
+	left(50)
+
+	for j in range (5):
+
+		forward (250)
+
+		left(1494)
